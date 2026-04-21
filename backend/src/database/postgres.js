0d840aed08f6e39387981734d510e7e4584637e4
Sebas220284 <sebastianvazquez928@gmail.com>
2026-04-21 11:40:36 -0600

    deploy

--- backend/src/database/postgres.js
+++ backend/src/database/postgres.js
@@ -3,15 +3,16 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
+  port: parseInt(process.env.DB_PORT || "5432"),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  // CRÍTICO: Railway requiere SSL para conexiones externas
   ssl: {
     rejectUnauthorized: false
   }
 });
-
+console.log("Intentando conectar a DB en:", process.env.DB_HOST);
 async function testDB() {
   try {
 
