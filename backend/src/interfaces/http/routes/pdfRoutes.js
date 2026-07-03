255b0a4c14f5261784e47abd24dfdf7d3da903df
Sebas220284 <96818637+Sebas220284@users.noreply.github.com>
2026-07-03 13:19:57 -0600

    Add route to get all items by dependency ID

--- backend/src/interfaces/http/routes/pdfRoutes.js
+++ backend/src/interfaces/http/routes/pdfRoutes.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/pdfController")
 
 router.post("/habilitar", controller.habilitar)
 router.get("/habilitados/:dependency_id", controller.getHabilitados)
+router.get("/todos/:dependency_id", controller.getTodos)
 
 module.exports = router
