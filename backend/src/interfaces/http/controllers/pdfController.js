702f9d2d42d51689218f7d63a0b83e6da7c08125
Sebas220284 <96818637+Sebas220284@users.noreply.github.com>
2026-07-03 12:02:11 -0600

    Actualizar pdfController.js
    Deploy

--- backend/src/interfaces/http/controllers/pdfController.js
+++ backend/src/interfaces/http/controllers/pdfController.js
@@ -2,27 +2,28 @@ const pool = require("../../../database/postgres")
 
 exports.habilitar = async (req, res) => {
   try {
-    const { dependency_id, anio, trimestre, habilitado_por } = req.body
+    const { dependency_id, anio, trimestre,habilitar,habilitado_por } = req.body
+    const isHabilitar= habilitar !== false;
 
     const result = await pool.query(`
-      INSERT INTO pdf_habilitados (dependency_id, anio, trimestre, habilitado_por)
-      VALUES ($1, $2, $3, $4)
+      INSERT INTO pdf_habilitados (dependency_id, anio, trimestre, habilitado, habilitado_por)
+      VALUES ($1, $2, $3, $4, $5)
       ON CONFLICT (dependency_id, anio, trimestre)
       DO UPDATE SET
-        habilitado = true,
+        habilitado = EXCLUDED.habilitado,
         habilitado_por = EXCLUDED.habilitado_por,
         fecha_habilitacion = NOW()
       RETURNING *
-    `, [dependency_id, anio, trimestre ?? null, habilitado_por ?? null])
+    `, [dependency_id, anio, trimestre ?? null, isHabilitar, habilitado_por ?? null])
 
     const data = result.rows[0]
-
-    req.app.get("io").to(dependency_id).emit("pdf_habilitado", {
+const eventName= isHabilitar ? "pdf_habilitado" : "pdf_desactivado";
+    req.app.get("io").to(dependency_id).emit(eventName, {
       anio: data.anio,
       trimestre: data.trimestre,
       mensaje: trimestre
-        ? `Tu PDF del T${trimestre}-${anio} fue habilitado`
-        : `Tu PDF del año ${anio} fue habilitado`
+        ? `Tu PDF del T${trimestre}-${anio} fue ${isHabilitar? 'habilitado  ' :  'desactivado  '} `
+        : `Tu PDF del año ${anio} fue  ${isHablitar ?  'habilitado  ' : 'desactivado '} `
     })
 
     res.json(data)
