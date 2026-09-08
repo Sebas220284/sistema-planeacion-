2437013f60fb0f4251f54671e33469aee9d3716f
Sebas220284 <sebastianvazquez928@gmail.com>
2026-09-08 09:25:24 -0600

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,8 +30,6 @@ const potRoutes = require("./interfaces/http/routes/potRoutes")
 const coloniasRoutes = require("./interfaces/http/routes/coloniasRoutes")
 const semaforoRoutes = require("./interfaces/http/routes/semaforoRoutes")
 const mppRoutes= require("./interfaces/http/routes/mppRoutes")
-const auditRoutes = require("./interfaces/http/routes/auditRoutes")
-const auditCtrl   = require("./interfaces/http/controllers/auditController")
 
 const app = express()
 
