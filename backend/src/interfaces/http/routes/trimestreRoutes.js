ca3e4156c9c43b8968fab228d4dcca0d5f867321
Leonardop711 <leonardo.perez68@unach.mx>
2026-09-12 10:18:27 -0600

    deploy roles

--- backend/src/interfaces/http/routes/trimestreRoutes.js
+++ backend/src/interfaces/http/routes/trimestreRoutes.js
@@ -2,9 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const controller = require("../controllers/trimestresController")
+const roleMiddleware = require("../middlewares/roleMiddleware")
 
 router.post("/guardar",controller.save)
 router.get("/porLinea/:planning_id",controller.getByLinea)
-router.put("/editar-directo",controller.editarDirecto)
+router.put("/editar-directo", roleMiddleware(["admin", "superadmin"]), controller.editarDirecto)
 router.get("/completo/:linea_id",controller.porLineaCompleto)
 module.exports = router
