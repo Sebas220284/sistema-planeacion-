52065eb4a62c8a4ecefe9bccb45c838500a47e78
Leonardop711 <leonardo.perez68@unach.mx>
2026-09-13 12:07:11 -0600

    deploy fix error audit

--- backend/src/interfaces/http/routes/auditRoutes.js
+++ backend/src/interfaces/http/routes/auditRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router  = express.Router()
 const ctrl    = require("../controllers/auditController")
-const { verificarToken, soloAdmin } = require("../middlewares/authMiddleware")
+const roleMiddleware = require("../middlewares/roleMiddleware")
 
-router.use(verificarToken)
-router.use(soloAdmin)
+// Token is already verified globally in server.js
+router.use(roleMiddleware(["admin", "superadmin"]))
 
 router.get("/",ctrl.listar)
 router.get("/stats", ctrl.getStats)
