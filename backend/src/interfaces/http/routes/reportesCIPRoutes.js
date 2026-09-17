43181b3f620e5579994ae8e214de83d04042557d
Sebas220284 <sebastianvazquez928@gmail.com>
2026-09-17 11:33:31 -0600

    deploy lineas de accion cip

--- backend/src/interfaces/http/routes/reportesCIPRoutes.js
+++ backend/src/interfaces/http/routes/reportesCIPRoutes.js
@@ -3,12 +3,7 @@ const router  = express.Router()
 const ctrl = require("../controllers/reportesCIPController")
 
 router.get("/cip/reporte1", ctrl.reporte1)
-<<<<<<< HEAD
 router.get("/cip/anios",    ctrl.getAnios)
 router.get("/cip/reporte2", ctrl.reporte2)
-=======
-router.get("/cip/reportePorEje", ctrl.reportePorEje)
-router.get("/cip/anios", ctrl.getAnios)
->>>>>>> 4dd486ee6aab31ccb2aa264898bb7a628317f2d5
 
 module.exports = router
